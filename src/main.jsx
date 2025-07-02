00f2a54cd7970cc205bae82dab5994a5ec3995cd
Rami Alhashmi <165218692+RamiRGH@users.noreply.github.com>
2025-07-02 03:01:33 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import App from './component/App'
+import Note from './component/Note'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <App />
+
+)
